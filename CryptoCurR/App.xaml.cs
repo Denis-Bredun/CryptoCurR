f91f5feff132b7bb26a3735763678db10d863e94
Denis_Bredun <bredun.denis@gmail.com>
2025-07-24 00:58:27 +0300

    Added DI mechanism using Microsoft.Extension packages

--- CryptoCurR/App.xaml.cs
+++ CryptoCurR/App.xaml.cs
@@ -1,14 +1,29 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace CryptoCurR
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+
+            Services = host.Services;
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
